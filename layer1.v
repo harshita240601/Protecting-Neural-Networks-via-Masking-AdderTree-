976077dc64731bdd9db520adb1e2b7f4c76c685e
Harshita Mishra <65452463+harshita240601@users.noreply.github.com>
2025-02-05 00:16:59 +0530

    Update layer1.v

--- layer1.v
+++ layer1.v
@@ -1,7 +1,5 @@
-module layer #(
-    parameter INPUT_SIZE = 10,
-    parameter OUTPUT_SIZE = 10,
-    parameter WIDTH = 16
+module layer1 #(
+    parameter INPUT_SIZE, OUTPUT_SIZE, WIDTH
 )(
     input reg [WIDTH-1:0] b [OUTPUT_SIZE-1:0],
 
@@ -20,4 +18,29 @@ module layer #(
     output reg [WIDTH-1:0] outputs [OUTPUT_SIZE-1:0]
 
 );
+
+    wire [WIDTH-1:0] weighted_sums [OUTPUT_SIZE-1:0];
+    
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws1 (.weights(w1), .inputs(inputs), .sum(weighted_sums[0]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws2 (.weights(w2), .inputs(inputs), .sum(weighted_sums[1]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws3 (.weights(w3), .inputs(inputs), .sum(weighted_sums[2]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws4 (.weights(w4), .inputs(inputs), .sum(weighted_sums[3]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws5 (.weights(w5), .inputs(inputs), .sum(weighted_sums[4]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws6 (.weights(w6), .inputs(inputs), .sum(weighted_sums[5]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws7 (.weights(w7), .inputs(inputs), .sum(weighted_sums[6]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws8 (.weights(w8), .inputs(inputs), .sum(weighted_sums[7]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws9 (.weights(w9), .inputs(inputs), .sum(weighted_sums[8]));
+    weighted_sum #(.INPUT_SIZE(INPUT_SIZE), .WIDTH(WIDTH)) ws10 (.weights(w10), .inputs(inputs), .sum(weighted_sums[9]));
+    
+    integer j;
+    always @(*) begin
+        for (j = 0; j < OUTPUT_SIZE; j = j + 1) begin
+            outputs[j] = weighted_sums[j] + b[j];
+        end
+    end
+endmodule
+
+
+
+    
 
